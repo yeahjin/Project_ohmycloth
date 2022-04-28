5bd411d3d2159d145a11ab10e67bfe70a849a20a
leesjin <leesjin@kookmin.ac.kr>
2022-04-28 16:20:31 +0900

    color test

--- Neohtgollajo/lib/config/palette.dart
+++ Neohtgollajo/lib/config/palette.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
 
 class Palette {
   static const Color iconColor = Color(0xFFB6C7D1);
@@ -8,4 +9,20 @@ class Palette {
   static const Color facebookColor = Color(0xFF3B5999);
   static const Color googleColor = Color(0xFF4D77FF);
   static const Color backgroudColor = Color(0xFF56BBF1);
+
+  // 아래는 메모 색 부분
+  static Color bgColor = Color(0xFFe2e2ff);
+  static Color mainColor = Color(0xFF000633);
+  static Color accentColor = Color(0xFF0065FF);
+
+  static List<Color> cardColor = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+  ];
 }
